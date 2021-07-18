77e86671eb30b2c4dc37b34679323b7e795db6f6
rrih <rsklv@icloud.com>
2021-07-19 01:28:55 +0900

    feat: 楽曲とスコアのテーブル暫定追加

--- docker/mysql/initdb.d/managedby_db.sql
+++ docker/mysql/initdb.d/managedby_db.sql
@@ -1,23 +1,51 @@
 CREATE DATABASE IF NOT EXISTS managedby_db;
 USE managedby_db;
 
+-- ユーザーの情報
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(64),
     email VARCHAR(64) NOT NULL,
     password VARCHAR(64) NOT NULL,
+    is_admin TINYINT(1) NOT NULL DEFAULT 0, -- admin であるか
+    deleted DATETIME,
     created DATETIME NOT NULL,
     modified DATETIME NOT NULL,
     PRIMARY KEY (id)
 );
 
--- TODO: 楽曲用テーブル作成
--- CREATE TABLE IF NOT EXISTS musics (
---     id INT NOT NULL AUTO_INCREMENT,
---     name VARCHAR(100),
+-- 楽曲の情報
+CREATE TABLE IF NOT EXISTS music (
+    id INT NOT NULL AUTO_INCREMENT,
+    registered_user_id INT NOT NULL,
+    name VARCHAR(100),
+    is_anime_video_dam TINYINT(1) NOT NULL DEFAULT 0, -- LIVE DAM の場合アニメ映像が存在するか
+    is_anime_video_joy TINYINT(1) NOT NULL DEFAULT 0, -- JOYSOUND の場合アニメ映像が存在するか
+    key_number INT,
+    is_fav TINYINT(1) NOT NULL DEFAULT 0,
+    deleted DATETIME,
+    created DATETIME NOT NULL,
+    modified DATETIME NOT NULL,
+    PRIMARY KEY (id)
+);
 
--- )
+-- スコアの情報
+CREATE TABLE IF NOT EXISTS score (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    another_user_id INT,
+    music_id INT NOT NULL,
+    is_dam TINYINT(1) NOT NULL DEFAULT 0, -- LIVE DAM であるか
+    is_joy TINYINT(1) NOT NULL DEFAULT 0, -- JOYSOUND であるか
+    score INT NOT NULL,
+    key_number INT,
+    text VARCHAR(1000),
+    deleted DATETIME,
+    created DATETIME NOT NULL,
+    modified DATETIME NOT NULL,
+    PRIMARY KEY (id)
+)
 
-INSERT INTO users (id, name, email, password, created, modified) VALUES (1, 'admin', 'rsklv@test.com', 'testtest', '2020-01-01 00:00:00', '2020-01-01 00:00:00');
-INSERT INTO users (id, name, email, password, created, modified) VALUES (2, 'origa', 'test@gmail.com', 'testtest', '2020-01-01 00:00:00', '2020-01-01 00:00:00');
-INSERT INTO users (id, name, email, password, created, modified) VALUES (3, 'ryohei', 'k.ryohei@test.co.jp', 'testtest', '2020-02-01 00:00:00', '2020-02-01 00:00:00');
+INSERT INTO users (id, name, email, password, is_admin, deleted, created, modified) VALUES (1, 'admin', 'rsklv@test.com', 'testtest', 0, null, '2020-01-01 00:00:00', '2020-01-01 00:00:00');
+INSERT INTO users (id, name, email, password, is_admin, deleted, created, modified) VALUES (2, 'origa', 'test@gmail.com', 'testtest', 0, null, '2020-01-01 00:00:00', '2020-01-01 00:00:00');
+INSERT INTO users (id, name, email, password, is_admin, deleted, created, modified) VALUES (3, 'ryohei', 'k.ryohei@test.co.jp', 'testtest', 0, null, '2020-02-01 00:00:00', '2020-02-01 00:00:00');
