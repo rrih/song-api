35a2d0e65c5f07ff7900a78f68d8c00f13221f60
rrih <rsklv@icloud.com>
2021-07-19 02:16:54 +0900

    feat: コメントとカテゴリーのテーブル追加

--- docker/mysql/initdb.d/managedby_db.sql
+++ docker/mysql/initdb.d/managedby_db.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(64) NOT NULL,
     password VARCHAR(64) NOT NULL,
     is_admin TINYINT(1) NOT NULL DEFAULT 0, -- admin であるか
-    deleted DATETIME,
+    deleted DATETIME DEFAULT NULL,
     created DATETIME NOT NULL,
     modified DATETIME NOT NULL,
     PRIMARY KEY (id)
@@ -18,14 +18,17 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS music (
     id INT NOT NULL AUTO_INCREMENT,
     registered_user_id INT NOT NULL,
-    name VARCHAR(100),
+    category_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    singer_name VARCHAR(100) NOT NULL, -- 歌手名
+    composer_name VARCHAR(100) NOT NULL, -- 作曲者名
+    source VARCHAR(100) NOT NULL, -- 楽曲を使用したコンテンツ名
     is_anime_video_dam TINYINT(1) NOT NULL DEFAULT 0, -- LIVE DAM の場合アニメ映像が存在するか
     is_anime_video_joy TINYINT(1) NOT NULL DEFAULT 0, -- JOYSOUND の場合アニメ映像が存在するか
     is_official_video_dam TINYINT(1) NOT NULL DEFAULT 0,
     is_official_video_joy TINYINT(1) NOT NULL DEFAULT 0,
-    is_fav TINYINT(1) NOT NULL DEFAULT 0,
-    start_singing VARCHAR(200),
-    deleted DATETIME,
+    start_singing VARCHAR(200), -- 歌い出し歌詞
+    deleted DATETIME DEFAULT NULL,
     created DATETIME NOT NULL,
     modified DATETIME NOT NULL,
     PRIMARY KEY (id)
@@ -35,21 +38,55 @@ CREATE TABLE IF NOT EXISTS music (
 CREATE TABLE IF NOT EXISTS score (
     id INT NOT NULL AUTO_INCREMENT,
     user_id INT NOT NULL,
-    another_user_id INT,
+    another_user_id INT, -- デュエットの場合
     music_id INT NOT NULL,
+    vibrato int NOT NULL DEFAULT 0, -- ビブラート
+    kobushi int NOT NULL DEFAULT 0, -- こぶし
+    fall int NOT NULL DEFAULT 0, -- フォール
+    shakuri int NOT NULL DEFAULT 0, -- しゃくり
     is_dam TINYINT(1) NOT NULL DEFAULT 0, -- LIVE DAM であるか
     is_joy TINYINT(1) NOT NULL DEFAULT 0, -- JOYSOUND であるか
     is_able_to_song_by_man TINYINT(1) NOT NULL DEFAULT 0, -- 男が歌える音程か
     is_able_to_song_by_woman TINYINT(1) NOT NULL DEFAULT 0, -- 女が歌える音程か
+    is_fav TINYINT(1) NOT NULL DEFAULT 0, -- お気に入りかどうか
     key_number INT, -- キー設定
     score FLOAT NOT NULL,
     key_number INT,
     text VARCHAR(1000),
-    deleted DATETIME,
+    deleted DATETIME DEFAULT NULL,
     created DATETIME NOT NULL,
     modified DATETIME NOT NULL,
     PRIMARY KEY (id)
-)
+);
+
+-- スコアとコメントの中間テーブル
+CREATE TABLE IF NOT EXISTS score_comments (
+    id INT NOT NULL AUTO_INCREMENT,
+    score_id INT NOT NULL,
+    comment_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- コメントの情報
+CREATE TABLE IF NOT EXISTS comments (
+    id INT NOT NULL AUTO_INCREMENT,
+    text VARCHAR(100) NOT NULL,
+    deleted DATETIME DEFAULT NULL,
+    created DATETIME NOT NULL,
+    modified DATETIME NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- カテゴリーの情報
+CREATE TABLE IF NOT EXISTS categories (
+    id INT NOT NULL AUTO_INCREMENT,
+    parent_id INT,
+    name VARCHAR(100) NOT NULL,
+    deleted DATETIME DEFAULT NULL,
+    created DATETIME NOT NULL,
+    modified DATETIME NOT NULL,
+    PRIMARY KEY (id)
+);
 
 INSERT INTO users (id, name, email, password, is_admin, deleted, created, modified) VALUES (1, 'admin', 'rsklv@test.com', 'testtest', 0, null, '2020-01-01 00:00:00', '2020-01-01 00:00:00');
 INSERT INTO users (id, name, email, password, is_admin, deleted, created, modified) VALUES (2, 'origa', 'test@gmail.com', 'testtest', 0, null, '2020-01-01 00:00:00', '2020-01-01 00:00:00');
