852f20ad31bfb00ebf2d15e0b40a4de82544d11c
rrih <rsklv@icloud.com>
2021-07-20 00:36:34 +0900

    modify: usersのカラム追加に対応

--- pkg/domain/entity/user_entity.go
+++ pkg/domain/entity/user_entity.go
@@ -2,10 +2,12 @@ package entity
 
 // TODO: あとで /pkg/domain 配下に移動する
 type User struct {
-	ID       int    `json: "id"`
-	Name     string `json: "string"`
-	Email    string `json: "string"`
-	Password string `json: "string"`
-	Created  string `json: "string"`
-	Modified string `json: "string"`
+	ID       int     `json: "id"`
+	Name     string  `json: "string"`
+	Email    string  `json: "string"`
+	Password string  `json: "string"`
+	IsAdmin  bool    `json: "bool"`
+	Deleted  *string `json: "string"`
+	Created  string  `json: "string"`
+	Modified string  `json: "string"`
 }
--- pkg/domain/repository/user_repository.go
+++ pkg/domain/repository/user_repository.go
@@ -7,11 +7,10 @@ import (
 	"github.com/rrih/managedby/pkg/infrastructure"
 )
 
-// TODO: repository から呼び出せるようにする
 func FindAll() []entity.User {
 	db := infrastructure.DbConn()
 	rows, err := db.Query(
-		"select id, name, email, password, created, modified from users",
+		"select id, name, email, password, is_admin, deleted, created, modified from users",
 	)
 	if err != nil {
 		log.Fatal(err.Error())
@@ -23,9 +22,11 @@ func FindAll() []entity.User {
 		var name string
 		var email string
 		var password string
+		var isAdmin bool
+		var deleted *string
 		var created string
 		var modified string
-		err := rows.Scan(&id, &name, &email, &password, &created, &modified)
+		err := rows.Scan(&id, &name, &email, &password, &isAdmin, &deleted, &created, &modified)
 		if err != nil {
 			panic(err)
 		}
@@ -33,6 +34,8 @@ func FindAll() []entity.User {
 		user.Name = name
 		user.Email = email
 		user.Password = password
+		user.IsAdmin = isAdmin
+		user.Deleted = deleted
 		user.Created = created
 		user.Modified = modified
 		users = append(users, user)
@@ -40,3 +43,17 @@ func FindAll() []entity.User {
 	defer db.Close()
 	return users
 }
+
+// func Add() entity.User {
+// 	db := infrastructure.DbConn()
+// 	time.Local = time.FixedZone("Asia/Tokyo", 9*60*60)
+// 	created := time.Now()
+// 	modified := time.Now()
+// 	rows, err := db.Query(
+// 		"insert into users (id, name, email, password, created, modified) values (?, ?, ?, ?)",
+// 	)
+// 	if err != nil {
+// 		log.Fatal(err.Error())
+// 	}
+
+// }
