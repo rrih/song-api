3aca270cb1eeab76c3857b0643c041e55611396a
rrih <rsklv@icloud.com>
2021-07-24 04:33:39 +0900

    refactor: ルーティング修正

--- pkg/usecase/user.go
+++ pkg/usecase/user.go
@@ -13,40 +13,50 @@ import (
 
 func FindUsers(w http.ResponseWriter, r *http.Request) {
 	// TODO: add cors
-	body := repository.FindAll()
-	// TODO: エラー時を想定していない
-	middleware.Response(w, nil, map[string]interface{}{"data": body})
+	if r.Method == "GET" {
+		body := repository.FindAll()
+		// TODO: エラー時を想定していない
+		middleware.Response(w, nil, map[string]interface{}{"data": body})
+	}
 }
 
 func FindUser(w http.ResponseWriter, r *http.Request) {
-	userId := strings.Trim(r.URL.Path, "/users/")
-	id, _ := strconv.Atoi(userId)
-	body := repository.FindById(id)
-	middleware.Response(w, nil, map[string]interface{}{"data": body})
+	if r.Method == "GET" {
+		userId := strings.Trim(r.URL.Path, "/users/view/")
+		id, _ := strconv.Atoi(userId)
+		body := repository.FindById(id)
+		middleware.Response(w, nil, map[string]interface{}{"data": body})
+	}
 }
 
 func AddUsers(w http.ResponseWriter, r *http.Request) {
-	var p entity.InsertedUser
-	json.NewDecoder(r.Body).Decode(&p)
-	repository.Insert(p)
-	// TODO: 保存されたデータを返す
+	if r.Method == "POST" {
+		var p entity.InsertedUser
+		json.NewDecoder(r.Body).Decode(&p)
+		repository.Insert(p)
+		// TODO: 保存されたデータを返す
+	}
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
-	// ユーザーIDを取得
-	userId := strings.Trim(r.URL.Path, "/users/update/")
-	id, _ := strconv.Atoi(userId)
-	// TODO: http メソッドが put であるかチェックする
-	// TODO: id 存在するユーザIDか存在しないユーザIDかでエラーハンドリングする
-	// TODO: 異常系
-	var p entity.InsertedUser
-	json.NewDecoder(r.Body).Decode(&p)
-	repository.Update(p, id)
-	// TODO: repository.Update の結果を response として返す
+	if r.Method == "PUT" {
+		// ユーザーIDを取得
+		userId := strings.Trim(r.URL.Path, "/users/update/")
+		id, _ := strconv.Atoi(userId)
+		// TODO: http メソッドが put であるかチェックする
+		// TODO: id 存在するユーザIDか存在しないユーザIDかでエラーハンドリングする
+		// TODO: 異常系
+		var p entity.InsertedUser
+		json.NewDecoder(r.Body).Decode(&p)
+		repository.Update(p, id)
+		// TODO: repository.Update の結果を response として返す
+	}
 }
 
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	userId := strings.Trim(r.URL.Path, "/users/delete/")
-	id, _ := strconv.Atoi(userId)
-	repository.LogicalDeleteUser(id)
+	if r.Method == "DELETE" {
+		userId := strings.Trim(r.URL.Path, "/users/delete/")
+		id, _ := strconv.Atoi(userId)
+		repository.LogicalDeleteUser(id)
+	}
 }
