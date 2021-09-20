63247ee98bbdd86376775d34e51b651c714ad350
rrih <rsklv@icloud.com>
2021-09-21 00:16:42 +0900

    fix: modify type for yes null, create repository file

--- pkg/domain/entity/category_entity.go
+++ pkg/domain/entity/category_entity.go
@@ -2,10 +2,10 @@ package entity
 
 // Category エンティティ
 type Category struct {
-	ID       string `json:"id"`
-	ParentID string `json:"parent_id"`
-	Name     string `json:"name"`
-	Deleted  string `json:"deleted"`
-	Created  string `json:"created"`
-	Modified string `json:"modified"`
+	ID       string  `json:"id"`
+	ParentID *string `json:"parent_id"`
+	Name     string  `json:"name"`
+	Deleted  *string `json:"deleted"`
+	Created  string  `json:"created"`
+	Modified string  `json:"modified"`
 }
