1c3a653904f272dc65253993d122682d98c2ec5d
rrih <rsklv@icloud.com>
2021-09-20 23:56:05 +0900

    add category entity file

---
+++ pkg/domain/entity/category_entity.go
@@ -0,0 +1,11 @@
+package entity
+
+// Category エンティティ
+type Category struct {
+	ID       string `json:"id"`
+	ParentID string `json:"parent_id"`
+	Name     string `json:"name"`
+	Deleted  string `json:"deleted"`
+	Created  string `json:"created"`
+	Modified string `json:"modified"`
+}
